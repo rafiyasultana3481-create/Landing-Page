1457cc00287077ba8ca120af989e035f4d72929f
Rafiya <rafiyasultana.3481@gmail.com>
2026-04-14 14:47:05 +0530

    changes made to css file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,18 @@
 function Hero() {
   return (
-    <section id="hero" className="min-vh-100 d-flex align-items-center text-center">
-      <div className="container">
-        <h1 className="text-uppercase text-white fw-semibold display-1">
-          Techno Tune Motors
-        </h1>
-        <h5 className="text-white mt-3 mb-4">
-          Discover a full spectrum of services at TechTune Motors
-        </h5>
-        <a href="#about" className="btn btn-brand">Read more</a>
-      </div>
-    </section>
+    <section id="hero" class="min-vh-100 d-flex align-items-center text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-uppercase text-white fw-semibold display-1">Techno Tune Motors</h1>
+                <h5 class="text-white mt-3 mb-4">Discover a full spectrum of services at TechTune Motors</h5>
+                <div>
+                    <a href="#about" class="btn btn-brand">Read more</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
   );
 }
 
